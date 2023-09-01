f7e15c22dff5934e01b9fa730cb10424fe7beb68
Sreeja194 <143074208+Sreeja194@users.noreply.github.com>
2023-09-01 16:07:35 +0530

    Add files via upload

--- CPU Scheduling Algorithms/fcfs.c
+++ CPU Scheduling Algorithms/fcfs.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+void main(){
+    int bt[20],wt[20],tat[20],i,n;
+    float wtavg,tatavg;
+    printf("Enter the number of processes:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter the burst time for process %d:",i);
+        scanf("%d",&bt[i]);
+    }
+    wt[0]=wtavg=1;
+    tat[0]=tatavg=bt[0];
+    for(i=1;i<n;i++){
+    	wt[i]=wt[i-1]+bt[i-1];
+    	tat[i]=tat[i-1]+bt[i];
+    	wtavg=wtavg+wt[i];
+    	tatavg=tatavg+tat[i];
+    }
+    printf("\tProcess\tBurst time\tWaiting Time\tTurnaround Time\n");
+    for(i=0;i<n;i++){
+    	printf("\n\t%d\t\t%d\t\t%d\t\t%d",i,bt[i],wt[i],tat[i]);
+    }
+    printf("\nAverage waiting time:%f",wtavg/n);
+    printf("\nAverage Turnaround time:%f",tatavg/n);
+}
