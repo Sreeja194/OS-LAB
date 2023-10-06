51dfbbc548c79a1b9ae28e8c1a4ce68fcd3969d3
Sreeja194 <143074208+Sreeja194@users.noreply.github.com>
2023-10-06 16:06:30 +0530

    Add files via upload

--- memory allocation/firstfit.c
+++ memory allocation/firstfit.c
@@ -2,16 +2,19 @@
 #define max 25
 void main()
 {
-	int  frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
+	int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0;
 	static int bf[max],ff[max];
+	printf("\n\tMemory Management Scheme - First Fit");
 	printf("\nEnter the number of blocks:");
 	scanf("%d",&nb);
 	printf("Enter the number of files:");
 	scanf("%d",&nf);
 	printf("\nEnter the size of the blocks:-\n");
 	for(i=1;i<=nb;i++)
+	{
 		printf("Block %d:",i);
 		scanf("%d",&b[i]);
+	}
 	printf("Enter the size of the files :-\n");
 	for(i=1;i<=nf;i++)
 	{
@@ -22,21 +25,22 @@ void main()
 	{
 		for(j=1;j<=nb;j++)
 		{
-			if(bf[j]!=1)
+			if(bf[j]!=1) 
 			{
 				temp=b[j]-f[i];
 				if(temp>=0)
-					if(lowest>temp)
+					if(highest<temp)
 					{
-						ff[i]=j;
-						lowest=temp;
+						frag[i]=highest;
+						bf[ff[i]]=1; 
+						highest=0;
 					}
 			}
 		}
-		frag[i]=lowest; bf[ff[i]]=1;
-		lowest=10000;
+		ff[i]=j; 
+		highest=temp;
 	}
-	printf("\nFile No\tFile Size \tBlock No\tBlock Size\tFragment"); 
-	for(i=1;i<=nf && ff[i]!=0;i++)
+	printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
+	for(i=1;i<=nf;i++)
 	printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 }
--- memory allocation/worstfit.c
+++ memory allocation/worstfit.c
@@ -2,16 +2,19 @@
 #define max 25
 void main()
 {
-	int  frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
+	int frag[max],b[max],f[max],i,j,nb,nf,temp;
 	static int bf[max],ff[max];
+	printf("\n\tMemory Management Scheme - Worst Fit");
 	printf("\nEnter the number of blocks:");
 	scanf("%d",&nb);
 	printf("Enter the number of files:");
 	scanf("%d",&nf);
 	printf("\nEnter the size of the blocks:-\n");
 	for(i=1;i<=nb;i++)
+	{
 		printf("Block %d:",i);
 		scanf("%d",&b[i]);
+	}
 	printf("Enter the size of the files :-\n");
 	for(i=1;i<=nf;i++)
 	{
@@ -26,17 +29,17 @@ void main()
 			{
 				temp=b[j]-f[i];
 				if(temp>=0)
-					if(lowest>temp)
-					{
-						ff[i]=j;
-						lowest=temp;
-					}
+				{
+					ff[i]=j;
+					break;
+				}
 			}
 		}
-		frag[i]=lowest; bf[ff[i]]=1;
-		lowest=10000;
+		frag[i]=temp;
+		bf[ff[i]]=1;
 	}
-	printf("\nFile No\tFile Size \tBlock No\tBlock Size\tFragment"); 
-	for(i=1;i<=nf && ff[i]!=0;i++)
+	printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
+	for(i=1;i<=nf;i++)
 	printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 }
+
